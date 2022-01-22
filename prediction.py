014c6588f1c36b4b1c477347690c887895fe4129
smo <shangsimou@gmail.com>
2022-01-22 21:15:23 +0800

    预测后修正yaw和pitch区间

--- prediction.py
+++ prediction.py
@@ -1,6 +1,8 @@
+import math
 
 from scipy import stats
 from statsmodels.tsa.ar_model import AutoReg
+import numpy as np
 
 from nntrain import *
 
@@ -10,6 +12,7 @@ def linear_model(x_parameter, y_parameter, predict_value):
     for i in range(len(y_parameter)):
         slope, intercept, r, p, std_err = stats.linregress(x_parameter, y_parameter[i])
         predictions.append([slope * j + intercept for j in predict_value])
+    predictions = standardize(predictions)
     return predictions
 
 
@@ -18,6 +21,7 @@ def avg_model(x_parameter, y_parameter, predict_value):
     for i in range(len(y_parameter)):
         mean = np.mean(y_parameter[i])
         predictions.append([0 * j + mean for j in predict_value])
+    predictions = standardize(predictions)
     return predictions
 
 
@@ -25,16 +29,30 @@ def ar_model(x_parameter, y_parameter, predict_value):
     predictions = []
     for i in range(len(y_parameter)):
         train = y_parameter[0]
-        model_fit = AutoReg(train,20).fit()
-        predictions.append(model_fit.predict(len(train),len(train)+len(predict_value)-1))
+        model_fit = AutoReg(train, 5).fit()
+        predictions.append(model_fit.predict(len(train), len(train) + len(predict_value) - 1))
+    predictions = standardize(predictions)
     return predictions
 
 
 def nnar_model(x_parameter, tr, net, all_users_trajectory):
-    predictions = [[],[]]
+    predictions = [[], []]
     input = get_input(x_parameter[0], all_users_trajectory, tr)
     tensor = net(Variable(torch.unsqueeze(torch.FloatTensor([input]), dim=1)))
     list = tensor.detach().numpy().tolist()[0][0]
     for i in range(len(list)):
-           predictions[i%2].append(list[i])
-    print(predictions)
+        predictions[i % 2].append(list[i])
+    predictions = standardize(predictions)
+    return predictions
+
+
+def standardize(predictions):
+    for i in range(len(predictions[0])):
+        tmp = predictions[0][i]
+        if math.sin(predictions[0][i]) >= 0:
+            predictions[0][i] = math.acos(math.cos(predictions[0][i]))
+        else:
+            predictions[0][i] = -math.acos(math.cos(predictions[0][i]))
+    for i in range(len(predictions[1])):
+        predictions[1][i] = math.asin(math.sin(predictions[1][i]))
+    return predictions
--- process.py
+++ process.py
@@ -1,9 +1,9 @@
 import csv
+import math
 
 import numpy as np
 
 from prediction import *
-
 from element import *
 
 dataSetPath = "./vr-dataset/Formated_Data/"
@@ -35,7 +35,8 @@ def init():
 def reader(experimentNo, videoNo, usrNo):
     viewpointList = []
     viewpointReader = csv.reader(
-        open(dataSetPath + experimentPath + str(experimentNo) + "/" + str(usrNo) + viewpointFile + str(videoNo) + dataSetType))
+        open(dataSetPath + experimentPath + str(experimentNo) + "/" + str(usrNo) + viewpointFile + str(
+            videoNo) + dataSetType))
     viewpointReader.__next__()
     for line in viewpointReader:
         quat = Quaternion(float(line[2]), float(line[3]), float(line[4]), float(line[5]))
@@ -64,12 +65,37 @@ def train_nn_net():
 
 if __name__ == '__main__':
     init()
-    train_nn_net()
+    users = []
+    for i in range(40):
+        users.append(i + 1)
+    net = Net(17, 20, 8)
+    net.load_state_dict(torch.load('./nnar_model/NNAR_2_3_706.pth'))
+    l = Trajectory(0.5, reader(2, 3, 42))
+    all_users_trajectory = read_users(2, 3, users)
+    accs_yaw = []
+    accs_pitch = []
+    for t in range(16,len(l.yaws) - 8):
+        #prediction = nnar_model([t], l, net, all_users_trajectory)
+        prediction = linear_model([t, t + 1, t + 2, t + 3], [l.yaws[t:t + 4], l.pitchs[t:t + 4]], [t + 4, t + 5, t + 6, t + 7])
+        #prediction = avg_model([t, t + 1, t + 2, t + 3], [l.yaws[t:t + 4], l.pitchs[t:t + 4]], [t + 4, t + 5, t + 6, t + 7])
+        #prediction = ar_model([t], [l.yaws[t-16:t + 4], l.pitchs[t-16:t + 4]], [t + 4, t + 5, t + 6, t + 7])
 
-    #net = Net(17, 20, 8)
-    #net.load_state_dict(torch.load('./nnar_model/NNAR_2_4_417.pth'))
-    #l = Trajectory(0.5, reader(2, 4, 46))
-    #t = 250
-    #nnar_model([t],l,net,read_users(2,4,users))
-    #print(l.yaws[t:t+4])
-    #print(l.pitchs[t:t+4])
+        accura_yaw = abs(l.yaws[t + 4] - prediction[0][0]) / (2 * math.pi)
+        if accura_yaw > 0.5:
+            accura_yaw = 1 - accura_yaw
+        accura_pitch = abs(l.pitchs[t + 4] - prediction[1][0]) / math.pi
+
+        accs_yaw.append(accura_yaw)
+        accs_pitch.append(accura_pitch)
+    print(np.mean(accs_yaw))
+    print(np.mean(accs_pitch))
+    import matplotlib.pyplot as plt
+
+    plt.plot(accs_yaw)
+    plt.show()
+    '''
+    print(l.yaws[t:t+4])
+    print(prediction[0])
+    print(l.pitchs[t:t+4])
+    print(prediction[1])
+    '''
